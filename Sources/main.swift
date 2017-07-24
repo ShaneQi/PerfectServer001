9e3c0d25a7bfbfe1adc2b37bb272c36433997164
ShaneQi <qizengtai@gmail.com>
2017-07-24 11:14:49 -0500

    Updated dateformatter APIs.

--- Sources/main.swift
+++ Sources/main.swift
@@ -30,41 +30,9 @@ routes.add(method: .get, uri: "/", handler: {
 	}
 )
 
-routes.add(method: .post, uri: "/stringfromdate", handler: {
+routes.add(method: .post, uri: "/dateformatter", handler: {
 	request, response in
 
-	response.status = HTTPResponseStatus.statusFrom(code: 400)
-	response.setHeader(.contentType, value: "application/json")
-	let bodyString = request.postBodyString ?? ""
-
-	do {
-		let bodyObject = try bodyString.jsonDecode()
-		if let bodyDictionary = bodyObject as? [String: Any], 
-				let format = bodyDictionary["format"] as? String
-			{
-				var responseDictionary = [String: Any]()
-
-				let formatter = DateFormatter()
-				formatter.dateFormat = format
-				
-				let dateString = formatter.string(from: Date())
-				responseDictionary["date_string"] = dateString
-
-				let responseString = try responseDictionary.jsonEncodedString()
-				response.status = HTTPResponseStatus.ok
-				response.appendBody(string: responseString)
-				response.completed()
-			 
-			} else {
-					response.completed()
-			}
-	} catch {
-		response.completed()
-	}
-})
-
-routes.add(method: .post, uri: "/datefromstring", handler: {
-	request, response in 
 	response.status = HTTPResponseStatus.statusFrom(code: 400)
 	response.setHeader(.contentType, value: "application/json")
 	let bodyString = request.postBodyString ?? ""
@@ -80,15 +48,18 @@ routes.add(method: .post, uri: "/datefromstring", handler: {
 				let formatter = DateFormatter()
 				formatter.dateFormat = format
 				
+				let dateStringResult = formatter.string(from: Date())
+				responseDictionary["date_string"] = dateStringResult
+
 				let date = formatter.date(from: dateString)
 				if let unwrappedDate = date {
 					let tempFormatter = DateFormatter()
 					tempFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
 					let dateString: String? = tempFormatter.string(from: unwrappedDate)
 					// 2000-01-01 00:00:00 +0000	
-					responseDictionary["date_string"] = "\(dateString)"
+					responseDictionary["date_value"] = "\(dateString)"
 				} else {
-					responseDictionary["date_string"] = date
+					responseDictionary["date_value"] = "nil"
 				}
 
 				let responseString = try responseDictionary.jsonEncodedString()
@@ -102,7 +73,6 @@ routes.add(method: .post, uri: "/datefromstring", handler: {
 	} catch {
 		response.completed()
 	}
-
 })
 
 server.addRoutes(routes)
