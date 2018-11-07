1732fd616a9dca5ba42111a128290432d5d6426e
Shane Qi <qizengtai@gmail.com>
2018-11-07 20:40:51 +0000

    Added docker network.

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,7 @@ docker run \
 -d \
 --name PerfectServer001 \
 -v `pwd`:/PerfectServer001 \
+--network GoldenArches \
 -w /PerfectServer001 \
 shaneqi/swift:4.0-DEVELOPMENT-SNAPSHOT-2017-09-30-a \
 /bin/sh -c \
